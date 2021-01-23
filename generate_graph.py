dece7650f0bae0bde6246b0a17a52cf49729d467
DUCHEMIN Emeric <educhemin001@enseirb-matmeca.fr>
2021-01-23 17:49:53 +0100

    Modification to accept array

--- generate_graph.py
+++ generate_graph.py
@@ -3,9 +3,9 @@ from matplotlib.pyplot import *
 
 with open('data.json') as json_file :
     data  = json.load(json_file)
-linsp1 = data.linsp1
-lisnp2 = data.linsp2
-tab_inf = data.tab_inf
+linsp1 = data[0]
+lisnp2 = data[1]
+tab_inf = data[2]
 for i in range(len(linsp1)) :
     plot(linsp2,tab_inf[i], label = "Proba_de detection : " +str(linsp1[i]))
 title('Graphe de d\'infectés en fonction de la probabilité de détection et d\'infection')
